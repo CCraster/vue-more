1c9f149aff684491c6b71ebd684fdc4b5dccf782
Craster <xiongxiaoxiao1@xiaomi.com>
2019-09-05 00:52:27 +0800

    fix(all): 修复build文件夹doc >> docs

--- config/index.js
+++ config/index.js
@@ -45,10 +45,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../doc/index.html'),  //之前是'../dist/index.html'
+    index: path.resolve(__dirname, '../docs/index.html'),  //之前是'../dist/index.html'
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../doc'),        // 之前是 '../dist'
+    assetsRoot: path.resolve(__dirname, '../docs'),        // 之前是 '../dist'
     assetsSubDirectory: 'static',
     assetsPublicPath: './',                               // 之前是 '/'
 
