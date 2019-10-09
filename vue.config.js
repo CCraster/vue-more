52323c34727e406161899ae093831977585b4dcf
Craster <704133029@qq.com>
2019-10-09 09:21:38 +0800

    feat(vue.config): 生产环境不生成source map

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 // vue.config.js
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? '/vue-more/' : '/', // 指定项目相对于服务器根地址的路径，此处设置是为了配合git pages
-    outputDir: 'docs' // 指定build的输出文件夹，git pages只能指定根目录 / docs文件夹作为资源文件夹
+    outputDir: 'docs', // 指定build的输出文件夹，git pages只能指定根目录 / docs文件夹作为资源文件夹
+    productionSourceMap: false
 };
